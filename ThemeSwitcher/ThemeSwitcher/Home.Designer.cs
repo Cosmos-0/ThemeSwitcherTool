88479b564ae3e6f22b829b64e0395725c723a796
oussama dalaa <oussamadalaa6@gmail.com>
2023-10-24 18:00:18 +0100

    adding project

---
+++ ThemeSwitcher/ThemeSwitcher/Home.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ThemeSwitcher
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            themeSwitch = new ReaLTaiizor.Controls.HopeSwitch();
+            notifyIcon = new System.Windows.Forms.NotifyIcon(components);
+            pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // themeSwitch
+            // 
+            themeSwitch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            themeSwitch.AutoSize = true;
+            themeSwitch.BaseColor = System.Drawing.Color.White;
+            themeSwitch.BaseOffColor = System.Drawing.Color.FromArgb(220, 223, 230);
+            themeSwitch.BaseOnColor = System.Drawing.Color.FromArgb(64, 158, 255);
+            themeSwitch.Location = new System.Drawing.Point(11, 12);
+            themeSwitch.Name = "themeSwitch";
+            themeSwitch.Size = new System.Drawing.Size(40, 20);
+            themeSwitch.TabIndex = 1;
+            themeSwitch.Text = "ThemeSwitcher";
+            themeSwitch.UseVisualStyleBackColor = true;
+            themeSwitch.CheckedChanged += themeSwitch_CheckedChanged;
+            // 
+            // notifyIcon
+            // 
+            notifyIcon.Icon = (System.Drawing.Icon)resources.GetObject("notifyIcon.Icon");
+            notifyIcon.Text = "Theme switcher ";
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += notifyIcon_MouseDoubleClick;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            pictureBox.Image = (System.Drawing.Image)resources.GetObject("pictureBox.Image");
+            pictureBox.InitialImage = null;
+            pictureBox.Location = new System.Drawing.Point(11, 1);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new System.Drawing.Size(15, 11);
+            pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 2;
+            pictureBox.TabStop = false;
+            pictureBox.MouseDown += pictureBox_MouseDown;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.Green;
+            ClientSize = new System.Drawing.Size(63, 44);
+            Controls.Add(pictureBox);
+            Controls.Add(themeSwitch);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            MaximizeBox = false;
+            MaximumSize = new System.Drawing.Size(1366, 728);
+            MinimizeBox = false;
+            Name = "Home";
+            ShowInTaskbar = false;
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Theme switcher";
+            TransparencyKey = System.Drawing.Color.Green;
+            Load += Home_Load;
+            Resize += Home_Resize;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private ReaLTaiizor.Controls.HopeSwitch themeSwitch;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.PictureBox pictureBox;
+
+        #endregion
+        //private ReaLTaiizor.Controls.ParrotFormHandle parrotFormHandle1;
+    }
+}
+
---
+++ ThemeSwitcher/ThemeSwitcher/Home.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ThemeSwitcher
+{
+    public partial class Home : Form
+    {
+
+        int SystemRes;
+        RegistryKey key;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        public Home()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            InitializeContextMenu();
+            key = Registry.CurrentUser.OpenSubKey("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", true);
+            SystemRes = (int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "SystemUsesLightTheme", -1);
+            themeSwitch.Checked = SystemRes.Equals(0);
+
+
+        }
+
+        private void themeSwitch_CheckedChanged(object sender, EventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "SystemUsesLightTheme", SystemRes.Equals(0) ? 1 : 0);
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "AppsUseLightTheme", SystemRes.Equals(0) ? 1 : 0);
+            SystemRes = SystemRes.Equals(0) ? 1 : 0;
+
+        }
+
+        private void Home_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();  // Hide the form
+                notifyIcon.Visible = true; // Show the system tray icon
+            }
+        }
+
+
+
+        private void notifyIcon_MouseDoubleClick(object sender, EventArgs e)
+        {
+            Show();  // Show the form
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = true; // Hide the system tray icon
+        }
+
+        private void InitializeContextMenu()
+        {
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+            ToolStripMenuItem MinimizeMenuItem = new ToolStripMenuItem("Minimize");
+            exitMenuItem.Click += (sender, e) => { Application.Exit(); };
+            MinimizeMenuItem.Click += (sender, e) => { this.Hide(); };
+            contextMenu.Items.Add(exitMenuItem);
+            contextMenu.Items.Add(MinimizeMenuItem);
+            notifyIcon.ContextMenuStrip = contextMenu;
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+    }
+}
